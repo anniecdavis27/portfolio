5233f73056085fb49457f274922b8f2bc0c8d00b
Annie Davis <anniecdavis27@gmail.com>
2020-06-12 12:05:34 -0400

    nav bar fixed

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 
 const hamburgerBtn = document.querySelector('.hamburger-btn')
 const dropDownDisplay = document.querySelector('.dropdown-menu')
-const dropDownLinks = document.querySelector('.nav-link')
+const dropDownLinkOne = document.querySelector('#nav-one')
+const dropDownLinkTwo = document.querySelector('#nav-two')
+const dropDownLinkThree = document.querySelector('#nav-three')
 
 let isMenuOpen = false;
 
@@ -16,14 +18,29 @@ hamburgerBtn.addEventListener('click', () => {
 })
 
 let isDropDown = false
+let isLinkOneVisible = false
+let isLinkTwoVisible = false
+let isLinkThreeVisible = false
 
 hamburgerBtn.addEventListener('click', () => {
     if (isDropDown == false) {
         dropDownDisplay.classList.add('show')
+        dropDownLinkOne.classList.add('visible')
+        dropDownLinkTwo.classList.add('visible-two')
+        dropDownLinkThree.classList.add('visible-three')
         isDropDown = true
+        isLinkOneVisible = true
+        isLinkTwoVisible = true
+        isLinkThreeVisible = true
     } else if (isDropDown == true) {
         dropDownDisplay.classList.remove('show')
+        dropDownLinkOne.classList.remove('visible')
+        dropDownLinkTwo.classList.remove('visible-two')
+        dropDownLinkThree.classList.remove('visible-three')
         isDropDown = false
+        isLinkOneVisible = false
+        isLinkTwoVisible = false
+        isLinkThreeVisible = false
     }
 })
 
